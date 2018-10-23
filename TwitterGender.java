21d0f17cb7cd94e20ac4657160c34557cd3032ac
dealrachaan <33548086+dealrachaan@users.noreply.github.com>
2018-10-23 14:06:38 +0100

    Create TwitterGender.java

---
+++ TwitterGender.java
@@ -0,0 +1,3 @@
+// read tweets from male and female Twitter users
+// denote (100? 1000?) most common words exclusive to male and female users
+// read future tweets by scanning for presence of these keywords
