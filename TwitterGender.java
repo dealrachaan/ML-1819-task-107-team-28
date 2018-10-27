faa940e51b6af8ca8b90b5ccddac6427dd415805
Ethan <32280147+Ethan35@users.noreply.github.com>
2018-10-27 04:49:20 +0100

    altered dataset and TwitterGender.java
    removed commas from tweets so each tweet is now its own comma separated value, and removed quote marks from the dataset to prevent the same word being considered multiple separate words depending on whether it was at the start, end or middle of the tweet
    TwitterGender.java now makes a list of the words used in only tweets by female users and not in those by male users, and vice-versa.

--- TwitterGender.java
+++ TwitterGender.java
@@ -1,23 +1,73 @@
-// read tweets from male and female Twitter users
-// denote (100? 1000?) most common words exclusive to male and female users
-// read future tweets by scanning for presence of these keywords
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-import java.util.*
+public class TwitterGender {
 
-public class Entry (word){
-    public String word = word;
-    public int amount = 1;
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner s = new Scanner(new File("/home/doom/Desktop/Ethan/MachineLearningThing/Data.csv"));
+		s.useDelimiter(",");
+		
+		ArrayList<String> genders = new ArrayList<String>();
+		ArrayList<String[]> tweets = new ArrayList<String[]>();
+		ArrayList<String> femaleTweetWords = new ArrayList<String>();
+		ArrayList<String> maleTweetWords = new ArrayList<String>();
+
+		int i = 1;
+		while(s.hasNext()){
+		
+			if(i%2!=0){
+				String gender = s.next();
+				genders.add(gender);
+
+			}
+				
+			else {
+				String tweet = s.next();
+				String[] tweetWords = tweet.split(" ");
+				tweets.add(tweetWords);
+			}
+				
+            i++;
+        }
+		s.close();
+
+		for(int j = 0; j<tweets.size(); j++){
+
+			String[] tweet = tweets.get(j);
+
+			for (String x : tweet) {
+				
+				if(j<5368) //5368 is the last female entry in the dataset
+					femaleTweetWords.add(x);
+				else
+					maleTweetWords.add(x);
+			}
+
+		}
+
+		ArrayList<String> exclusivelyFemaleWords = new ArrayList<String>();
+		ArrayList<String> exclusivelyMaleWords = new ArrayList<String>();
+
+		for (String w :femaleTweetWords) {
+
+			if(!maleTweetWords.contains(w))
+				exclusivelyFemaleWords.add(w);
+			
+		}
+
+		for (String w :maleTweetWords) {
+
+			if(!femaleTweetWords.contains(w))
+				exclusivelyMaleWords.add(w);
+			
+		}
+
+		System.out.println(femaleTweetWords.size() + " words used by women");
+		System.out.println(maleTweetWords.size() + " words used by men");
+		System.out.println(exclusivelyFemaleWords.size() + " words used only by women");
+		System.out.println(exclusivelyMaleWords.size() + " words used only by men");
+		
+	}
+
 }
-
-//String of 100 most common words in English from https://gist.github.com/gravitymonkey/2406023
-String[] common = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"};
-
-//female data: 2D ArrayList of entry objects
-//for each word: if not in common, increment Entry count of word or make new Entry
-ArrayList<String> fList=new ArrayList<Entry>();
-//get word
-if word 
-fList.add(Entry(word)); 
-
-//male data: 2D ArrayList of entry objects
-//for each word: if not in common, increment Entry count of word or make new Entry
